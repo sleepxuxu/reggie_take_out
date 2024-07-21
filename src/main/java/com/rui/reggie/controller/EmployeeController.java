945186c7619bbd93c607bf421a893de03e6d7bfe
xurui <xurui_1116@outlook.com>
2024-07-21 15:34:56 +0800

    分页查询

--- src/main/java/com/rui/reggie/controller/EmployeeController.java
+++ src/main/java/com/rui/reggie/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.rui.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rui.reggie.common.Result;
 import com.rui.reggie.entity.Employee;
 import com.rui.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
@@ -92,5 +94,43 @@ public class EmployeeController {
 
         employeeService.save(employee);
         return Result.success("新增成功");
+    }
+
+    /**
+     * 员工分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+
+    @GetMapping("/page")
+    public Result<Page> page(int page, int pageSize, String name) {
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+        // 构造条件
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName, name);
+
+        // 添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        // 执行查询
+        employeeService.page(pageInfo, queryWrapper);
+
+        return Result.success(pageInfo);
+    }
+
+    @PostMapping("/update")
+    public Result<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateUser(empId);
+
+        employeeService.updateById(employee);
+
+        return Result.success("修改成功");
     }
 }
